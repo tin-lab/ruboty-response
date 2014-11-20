c33539532d4801f36cf98a01d77a1481a37a7774
kaihar4 <kaihar4@gmail.com>
2014-11-20 14:49:55 +0900

    Revert "Bug revise"
    This reverts commit 85d7f04c31c30215ec6aa3ab8d73aac0f5bc35ee.

--- lib/ruboty/handlers/response.rb
+++ lib/ruboty/handlers/response.rb
@@ -16,7 +16,7 @@ module Ruboty
           # If the response is a code
           if hash[:response] =~ /\A`.+`\z/
             thread = Thread.start do
-              result = `#{hash[:response].match(/\A`(.+)`\z/)[1]}`.chomp!.strip!
+              result = `#{hash[:response].match(/\A`(.+)`\z/)[1]}`.chomp!
               message.reply(result)
             end
             Thread.start do
